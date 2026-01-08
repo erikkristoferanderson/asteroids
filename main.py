b32a9043c93604530027bf75383c1ebc75fdaf45
Erik Anderson <erik.kristofer.anderson@gmail.com>
2026-01-08 01:06:31 -0600

    shoot asteroid to destroy

--- main.py
+++ main.py
@@ -49,6 +49,14 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
+
 
 
         screen.fill("black")
