d92f30cd925645329378debf2f8709d461a6497a
Erik Anderson <erik.kristofer.anderson@gmail.com>
2026-01-07 23:13:46 -0600

    use pygame groups

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        # player.update(dt)
+        updateable.update(dt)
+        for one_drawable in drawable:
+            one_drawable.draw(screen)
 
         screen.fill("black")
         player.draw(screen)
