4b205a7c9fa8c0100a76d4987bc5619da943c435
Erik Anderson <erik.kristofer.anderson@gmail.com>
2026-01-08 01:00:59 -0600

    rate limit the shots

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SHOOT_COOLDOWN_SECONDS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
 from shot import Shot
 
 class Player(CircleShape):
@@ -8,6 +8,8 @@ class Player(CircleShape):
         pass
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
+
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
@@ -23,6 +25,11 @@ class Player(CircleShape):
 
 
     def shoot(self):
+        if self.shot_cooldown_timer > 0:
+            return
+        self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+
+
         shot = Shot(self.position.x, self.position.y)  # TODO this is kinda messy, but let's see if it works
         start = pygame.Vector2((0, 1))
         rotated = start.rotate(self.rotation)
@@ -34,6 +41,9 @@ class Player(CircleShape):
 
 
     def update(self, dt):
+        self.shot_cooldown_timer -= dt 
+
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
